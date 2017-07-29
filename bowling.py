5db5d0379b696b74d2bc250df1728a564dec56af
janos.csjernyik <janos.csjernyik@gmail.com>
2017-07-29 14:15:33 +0200

    get_value(char) modified

--- bowling.py
+++ bowling.py
@@ -7,15 +7,15 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
@@ -30,13 +30,9 @@ def score(game):
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char in ['X', 'x', '/']:
         return 10
     elif char == '-':
         return 0
