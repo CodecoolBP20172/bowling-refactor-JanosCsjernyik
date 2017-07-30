4d2ae37869cc067691be809cc5b22ec1111a5073
janos.csjernyik <janos.csjernyik@gmail.com>
2017-07-30 20:04:38 +0200

    score(game) function simplified +names added

--- bowling.py
+++ bowling.py
@@ -2,24 +2,24 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+    strike = ['X', 'x']
+    spare = ['/']
     for i in range(len(game)):
-        if game[i] == '/':
+        if game[i] in spare:
             result += 10 - last + get_value(game[i + 1])
         else:
             result += get_value(game[i])
-        if frame < 10 and game[i] in ['X', 'x']:
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
-                else:
-                    result += get_value(game[i + 2])
-        if not in_first_half:
+        if frame < 10 and game[i] in strike:
+            result += get_value(game[i + 1])
+            if game[i + 2] in spare:
+                result += 10 - get_value(game[i + 1])
+            else:
+                result += get_value(game[i + 2])
+            in_first_half = True
             frame += 1
         if in_first_half:
             in_first_half = False
         else:
-            in_first_half = True
-        if game[i] in ['X', 'x']:
             in_first_half = True
             frame += 1
         last = get_value(game[i])
